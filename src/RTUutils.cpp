33d53c200129c29d3b4404f20a66c4217da91351
davidstyrbjorn <david.styrbjorn@qvantum.com>
2025-10-01 10:13:21 +0200

    crc modifieable lvalue fix

--- src/RTUutils.cpp
+++ src/RTUutils.cpp
@@ -81,7 +81,7 @@ uint16_t RTUutils::calcCRC(const uint8_t *data, uint16_t len, int offset) {
   uint8_t crcHi = 0xFF;
   uint8_t crcLo = 0xFF;
 
-  uint8_t _data[len-1];
+  uint8_t *_data = static_cast<uint8_t*>(malloc(len-1));
   memcpy(_data, data+1, len-1);
   len -= 1;
 
